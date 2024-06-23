14aff58b37a61956f2d4fcd52ebe33dbadceb46f
Caio Costa de Souza Lima <caiolimaa2002@gmail.com>
2024-06-23 14:46:52 -0300

    Update function.jsx

--- frontend/src/pages/Community/Chat/function/function.jsx
+++ frontend/src/pages/Community/Chat/function/function.jsx
@@ -479,9 +479,10 @@ const Function = () => {
                 setUnreadMessages((prevUnread) => prevUnread + 1);
                 fetchUserProfileImage(newMessage.userId);
                 // Adiciona a mensagem apenas se não for duplicada
-                if (!messages.find((msg) => msg.message === newMessage.message)) {
+                if (
+                  !messages.find((msg) => msg.message === newMessage.message)
+                ) {
                   setMessages((prevMessages) => [...prevMessages, newMessage]);
-                  setScrollToBottomNeeded(true); // Atualiza para rolar até o final
                 }
               }
             }
@@ -496,7 +497,7 @@ const Function = () => {
             console.error("Erro ao analisar a mensagem JSON:", error.message);
             return;
           }
-  
+
           // Verifica se a mensagem recebida não é um ping
           if (newMessage.type !== "ping") {
             // Verifica se a mensagem recebida não é do usuário atual
@@ -506,14 +507,13 @@ const Function = () => {
               // Adiciona a mensagem apenas se não for duplicada
               if (!messages.find((msg) => msg.message === newMessage.message)) {
                 setMessages((prevMessages) => [...prevMessages, newMessage]);
-                setScrollToBottomNeeded(true); // Atualiza para rolar até o final
               }
             }
           }
         }
       };
     }
-  }, [ws, userId, messages]);
+  }, [ws, userId]);
 
   useEffect(() => {
     scrollToBottomIfNeeded();
